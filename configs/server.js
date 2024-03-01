aeb22caf9af831790071081b263241900ea77907
Informatica <informatica@alumno.local>
2024-03-01 11:21:29 -0600

    Subiendo Login funcional

--- configs/server.js
+++ configs/server.js
@@ -6,15 +6,14 @@ import helmet from 'helmet'
 import morgan from 'morgan'
 import { dbConnection } from './mongo.js';
 import  userRoutes  from '../src/users/user.routes.js';
-
-
-
+import authRoutes from '../src/auth/auth.routes.js';
 
 class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
         this.usuarioPath = '/gestorSystem/v1/users';
+        this.authPath = '/gestorSystem/v1/auth';
 
         this.middlewares();
         this.conectarDB();
@@ -35,6 +34,7 @@ class Server{
 
     routes(){
         this.app.use(this.usuarioPath, userRoutes);
+        this.app.use(this.authPath, authRoutes );
     }
 
     listen(){
