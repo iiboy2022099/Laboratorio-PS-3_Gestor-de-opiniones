7f102966ad6c0187df8ea4248cf8f9a09f1f2afd
Informatica <informatica@alumno.local>
2024-03-01 07:54:54 -0600

    Subiendo get de usuarios

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -2,8 +2,25 @@ import { response, request } from "express";
 import bcryptjs from 'bcryptjs';
 import User from './user.model.js';
 
-export const userPost  = async (req, res) => {
-    const { nombre, correo, password} = req.body;
+export const getUsers = async (req, res) => {
+    const { limite, desde } = req.query;
+    const query = { estado: true };
+
+    const [total, users] = await Promise.all([
+        User.countDocuments(query),
+        User.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite)),
+    ]);
+
+    res.status(200).json({
+        total,
+        users,
+    });
+}
+
+export const userPost = async (req, res) => {
+    const { nombre, correo, password } = req.body;
     const user = new User({ nombre, correo, password });
     const salt = bcryptjs.genSaltSync();
     user.password = bcryptjs.hashSync(password, salt);
--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import {
-    userPost 
+    userPost,
+    getUsers 
 } from "./user.controller.js";
 import {
     existenteEmail,
@@ -10,6 +11,8 @@ import { validarCampos } from "../middlewares/validar-campos.js";
 
 
 const router = Router();
+
+router.get("/", getUsers);
 
 router.post(
     "/",
