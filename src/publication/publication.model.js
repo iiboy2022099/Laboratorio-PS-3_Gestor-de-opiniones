a89c203a829095427cb4ec9d5534aafd7b2b8154
Ignacio Iboy <iiboy-2022099@kinal.edu.gt>
2024-03-03 13:18:08 -0600

    Subiendo modelo de publication

---
+++ src/publication/publication.model.js
@@ -0,0 +1,25 @@
+import { Schema,model } from "mongoose";
+
+const publicationSchema = Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    }
+},{
+    versionKey: false
+})
+
+export default model('publication',publicationSchema)
